75a7ef92afafd75d98dc108dcdd45866bf66cb2e
mdrifatulislam <mdrifatulislam1080@gmail.com>
2025-08-06 14:15:59 +0600

    added reverse.js

---
+++ reverse.js
@@ -0,0 +1,23 @@
+// const newNumbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// const reversed = [];
+// for (let t = 0; t < newNumbers.length; t++) {
+//   console.log(newNumbers[t]);
+//   reversed.unshift(newNumbers[t]);
+// }
+// console.log(reversed);
+
+// for (let i = newNumbers.length; i >= 0; i--) {
+//   reversed.push(newNumbers[i]);
+// }
+// console.log(reversed);
+
+// const numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// const reversed = [];
+// for (const num of numbers) {
+//   reversed.unshift(num);
+//   console.log(reversed);
+// }
+
+const numbers = [20, 29, 79, 80, 49];
+numbers.reverse();
+console.log(numbers);
