00995fe76782fb6755f48fb66b5f9216e3d025d2
mdrifatulislam <mdrifatulislam1080@gmail.com>
2025-08-06 13:31:38 +0600

    added array-more.js

---
+++ array-more.js
@@ -0,0 +1,26 @@
+const fruits = [
+  "Fruits:",
+  "apple",
+  "banana",
+  "coconut",
+  "water melon",
+  "jackfruit",
+  "mango",
+];
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+const numbers = ["numbers:", 8, 99, 967, 344, 99, 99, 99, 960];
+
+for (let j = 0; j < numbers.length; j++) {
+  console.log(numbers[j]);
+}
+
+const prices = ["prices:", 698, 434, 564, 453, 33423, 6433];
+let s = 0;
+while (s < prices.length) {
+  console.log(prices[s]);
+  s++;
+}
