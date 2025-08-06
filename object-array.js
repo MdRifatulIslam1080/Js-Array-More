62948e9124146219c0fea09562414a5116ac3eb8
mdrifatulislam <mdrifatulislam1080@gmail.com>
2025-08-06 20:29:21 +0600

    added object-array.js

---
+++ object-array.js
@@ -0,0 +1,9 @@
+const employes = [
+  { name: "Rifat", age: 20, designation: "web deb", salary: 50000 },
+  { name: "Sabbir", age: 30, designation: "developer", salary: 30000 },
+  { name: "Mobin", age: 50, designation: "clark", salary: 10000 },
+];
+
+console.log(employes[0]);
+console.log(employes[1].salary);
+console.log("Rifat Salary:", employes[0].salary);
