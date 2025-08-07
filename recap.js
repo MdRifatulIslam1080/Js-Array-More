7020395c87004460adb981b323c00f1926447102
mdrifatulislam <mdrifatulislam1080@gmail.com>
2025-08-07 14:36:59 +0600

    added recap.js

---
+++ recap.js
@@ -0,0 +1,9 @@
+const books = ["Physics", "Chemistry", "Math", "Biology", "English", "Bangla"];
+for (let i = 0; i < books.length; i++) {
+  console.log(books[i]);
+}
+
+const rev_books = books.reverse();
+console.log(rev_books);
+const sorted_books = books.sort();
+console.log(sorted_books);
